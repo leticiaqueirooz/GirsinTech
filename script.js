afc635f16207004cdd9bb9af0669a7f021e2f126
leticiaqueirooz <101012123+leticiaqueirooz@users.noreply.github.com>
2022-04-17 22:50:05 +0100

    Add files via upload
    CA final - Letícia and Marina at NCI

---
+++ script.js
@@ -0,0 +1,76 @@
+//Some balls colors
+const colors = ["#FFF", "#d91694", "#517d57", "#0c3a96", "#F85F36", "#BBE3A3"];
+const numBalls = 100;
+const balls = [];
+
+for (let i = 0; i < numBalls; i++) {
+  let ball = document.createElement("div");
+  ball.classList.add("ball");
+  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+  ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+  ball.style.transform = `scale(${Math.random()})`;
+  ball.style.width = `${Math.random()}em`;
+  ball.style.height = ball.style.width;
+
+  balls.push(ball);
+  document.body.append(ball);
+}
+
+// Keyframes
+balls.forEach((el, i, ra) => {
+  let to = {
+    x: Math.random() * (i % 2 === 0 ? -11 : 11),
+    y: Math.random() * 12,
+  };
+
+  let anim = el.animate(
+    [
+      { transform: "translate(0, 0)" },
+      { transform: `translate(${to.x}rem, ${to.y}rem)` },
+    ],
+    {
+      duration: (Math.random() + 1) * 2000, // color duration
+      direction: "alternate",
+      fill: "both",
+      iterations: Infinity,
+      easing: "ease-in-out",
+    }
+  );
+});
+
+var myInput = document.getElementById("psw");
+var capital = document.getElementById("capital");
+var length = document.getElementById("length");
+
+// When the user clicks on the password field, show the message box
+myInput.onfocus = function () {
+  document.getElementById("message").style.display = "block";
+};
+
+// When the user clicks outside of the password field, hide the message box
+myInput.onblur = function () {
+  document.getElementById("message").style.display = "none";
+};
+
+// When the user starts to type something inside the password field
+myInput.onkeyup = function () {
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if (myInput.value.match(upperCaseLetters)) {
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  // Validate length
+  if (myInput.value.length >= 9) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+};
